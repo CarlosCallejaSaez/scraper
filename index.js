a36106e8c8dd536cb15c41b8086ca0c5d41c2763
CarlosCallejaSaez <carloscallejasez@gmail.com>
2023-07-23 18:53:26 +0200

    amazon title/price/image

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 const puppeteer = require("puppeteer");
-const jsdom = require("jsdom");
 
-const scraper= async () => {
-  try {
-    
-    const browser = await puppeteer.launch({
-      devtools: true,
-      defaultViewport: {
-        width: 1280,
-        height: 1024,
-      },
-      headless: false,
+const QUERY_SEARCH = "estoicismo"
+
+const scrapeProducts = async (query) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://www.amazon.com/");
+  await page.type("#twotabsearchtextbox", query);
+  await page.click("#nav-search-submit-text");
+  await page.waitForNavigation();
+  const products = await page.evaluate(() => {
+    const results = [];
+    const items = document.querySelectorAll(".s-result-item .s-card-border");
+
+    items.forEach((item) => {
+      const title = item.querySelector("h2 > a > span");
+      const price = item.querySelector(".a-price-whole");
+      const cents = item.querySelector(".a-price-fraction");
+      const image = item.querySelector("img");
+
+      if (!title || !price || !image) return;
+
+      results.push({
+        title: title.innerText,
+        price: parseFloat(
+          `${parseInt(price.innerText)}.${parseInt(cents.innerText)}`
+        ),
+        image: image.getAttribute("src"),
+      });
     });
-    const page = await browser.newPage();
-     await page.goto("https://www.zalando.es/", {
-      waitUntil: "domcontentloaded",
-    });
-     
-    
-    await page.type('#header-search-input', 'nike')
-    await page.keyboard.press('Enter');
-
-
-      
-  } catch (error) {
-    console.error(error);
-  }
+    return results;
+  });
+  console.log(products);
+  await browser.close();
 };
 
-scraper()
+scrapeProducts(QUERY_SEARCH);
